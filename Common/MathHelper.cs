8e1b7e745c6e7e12659dd98e9338b72336b44705
silentDoc <ramon78t@yahoo.es>
2024-11-29 22:36:23 +0100

    Prepared main structure, project folders and common libraries

---
+++ Common/MathHelper.cs
@@ -0,0 +1,13 @@
+﻿namespace AoC24.Common
+{
+    public static class MathHelper
+    {
+        // Greatest Common Divisor
+        public static long GCD(long num1, long num2)
+            => (num2 == 0) ? num1 : GCD(num2, num1 % num2);
+
+        // Least Common Multiple
+        public static long LCM(List<long> numbers)
+            => numbers.Aggregate((long S, long val) => S * val / GCD(S, val));
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,29 @@
-﻿namespace AoC24
+﻿using System.Diagnostics;
+
+namespace AoC24
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int day = 11;
+            int part = 1;
+            bool test = !false;
+            int testNum = 0;
+
+            string input = "./Input/day" + day.ToString("00");
+            input += (test) ? "_test" + (testNum > 0 ? testNum.ToString() : "") + ".txt" : ".txt";
+
+            Console.WriteLine("AoC 2024 - Day {0} , Part {1} - Test Data {2}", day, part, test);
+            Stopwatch st = new();
+            st.Start();
+            string result = day switch
+            {
+                _ => throw new ArgumentException("Wrong day number - unimplemented")
+            };
+            st.Stop();
+            Console.WriteLine("Result : {0}", result);
+            Console.WriteLine("Elapsed : {0}", st.Elapsed.TotalSeconds);
         }
     }
 }
