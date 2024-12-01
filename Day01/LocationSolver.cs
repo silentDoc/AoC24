16147a4c6933c603f30a9ccd6a3e9fae6a0c76ca
silentDoc <ramon78t@yahoo.es>
2024-12-01 09:47:35 +0100

    Day 1 - Part 2 Completed. Similarity score

--- Day01/LocationSolver.cs
+++ Day01/LocationSolver.cs
@@ -2,20 +2,23 @@
 {
     internal class LocationSolver
     {
-        List<int> Locations0 = new();
-        List<int> Locations1 = new();
+        List<int> left = new();
+        List<int> right = new();
 
         public void ParseInput(List<string> input)
         { 
             var values = input.Select(x => x.Split("   ").Select(int.Parse).ToList()).ToList();
-            Locations0 = values.Select(x => x[0]).OrderBy(x => x).ToList();
-            Locations1 = values.Select(x => x[1]).OrderBy(x => x).ToList();
+            left = values.Select(x => x[0]).OrderBy(x => x).ToList();
+            right = values.Select(x => x[1]).OrderBy(x => x).ToList();
         }
 
         int FindTotalDistance()
-            => Locations0.Zip(Locations1, (x, y) => Math.Abs(y - x)).Sum();
+            => left.Zip(right, (x, y) => Math.Abs(y - x)).Sum();
+
+        int SimilarityScore()
+            => left.Distinct().Select(x => right.Count(el => el==x) * x).Sum();
 
         public int Solve(int part = 1)
-            => FindTotalDistance();
+            => part == 1? FindTotalDistance() : SimilarityScore();
     }
 }
