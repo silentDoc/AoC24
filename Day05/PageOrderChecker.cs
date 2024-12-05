f1e2ee4df34287eb447b75cf18402127baa68f0a
silentDoc <ramon78t@yahoo.es>
2024-12-05 11:49:20 +0100

    Day 5 - Part 2 Completed. Using custom comparers

--- Day05/PageOrderChecker.cs
+++ Day05/PageOrderChecker.cs
@@ -1,5 +1,4 @@
 ﻿using AoC24.Common;
-using System.Security.Cryptography.X509Certificates;
 
 namespace AoC24.Day05
 {
@@ -7,6 +6,7 @@ namespace AoC24.Day05
     {
         List<List<int>> rules = [];
         List<List<int>> updates = [];
+        Comparer<int> customComparer;
 
         public void ParseInput(List<string> input)
         {
@@ -14,27 +14,33 @@ namespace AoC24.Day05
 
             sections[0].ForEach(s => rules.Add(s.Split('|').Select(int.Parse).ToList()));
             sections[1].ForEach(s => updates.Add(s.Split(',').Select(int.Parse).ToList()));
+            customComparer = CreatePageComparer();
         }
 
+        Comparer<int> CreatePageComparer()
+            => Comparer<int>.Create((a, b) => rules.Any(rule => rule[0] == a && rule[1] == b) ? -1
+                                              : rules.Any(rule => rule[0] == b && rule[1] == a) ? 1
+                                              : 0);
+
         bool IsCorrect(List<int> update)
-        { 
-            var rulesToApply = rules.Where(r => update.Contains(r[0]) && update.Contains(r[1]))
-                                                      .Select(r => update.IndexOf(r[0]) < update.IndexOf(r[1]))
-                                                      .ToList();
-            return rulesToApply.All(x => x);
-        }
+            => rules.Where(r => update.Contains(r[0]) && update.Contains(r[1]))
+                    .Select(r => update.IndexOf(r[0]) < update.IndexOf(r[1]))
+                    .All(x => x);
 
         int FindCorrectUpdates()
         {
             var correctUpdates = updates.Where(u => IsCorrect(u)).ToList();
-            var retVal = 0;
-            foreach (var update in correctUpdates)
-                retVal += update[update.Count() / 2];
-            
-            return retVal;
+            return correctUpdates.Sum(x => x[x.Count() / 2]);
+        }
+
+        int ReorderIncorrect()
+        {
+            var incorrectUpdates = updates.Where(u => !IsCorrect(u)).ToList();
+            var fixedUpdates = incorrectUpdates.Select(x => x.OrderBy(y=>y, customComparer).ToList()).ToList();
+            return fixedUpdates.Sum(x => x[x.Count() / 2]);
         }
 
         public int Solve(int part = 1)
-            => FindCorrectUpdates();
+            => part == 1 ? FindCorrectUpdates() : ReorderIncorrect();
     }
 }
