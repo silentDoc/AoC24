0c530a845166ece37032e3766ab58079b994b175
silentDoc <ramon78t@yahoo.es>
2024-12-07 08:53:55 +0100

    Day 7 - Part 1 Completed. Doing some maths

---
+++ Day07/RopeBridgeCalibrator.cs
@@ -0,0 +1,75 @@
+﻿namespace AoC24.Day07
+{
+    class Calibration
+    {
+        public long TestValue;
+        public List<long> Numbers;
+        List<List<char>> Operators = new();
+
+        public Calibration(string inputLine)
+        { 
+            var vals = inputLine.Replace(":", "").Split(' ').Select(long.Parse).ToList();
+            TestValue = vals[0];
+            Numbers = vals.Skip(1).ToList();
+        }
+
+        public bool Eval(List<char> operators)
+        {
+            var acum = Numbers[0];
+
+            for (int i = 1; i < Numbers.Count; i++)
+                acum = operators[i-1] == '+' ? acum + Numbers[i] : acum * Numbers[i];
+
+            return acum == TestValue;
+        }
+
+        public int FindWaysToSolve()
+        {
+            // Calc all the possible combinations
+
+            List<List<char>> ops = new();
+            for (int i = 0; i < Numbers.Count-1; i++)
+            {
+                if (ops.Count == 0)
+                {
+                    ops.Add(['+']);
+                    ops.Add(['*']);
+                    continue;
+                }
+
+                List<List<char>> newList = new();
+                foreach (var l in ops)
+                { 
+                    var tmp = l.ToList();
+                    var tmp2 = l.ToList();
+
+                    tmp.Add('+');
+                    tmp2.Add('*');
+
+                    newList.Add(tmp);
+                    newList.Add(tmp2);
+
+                    ops = newList;
+                }
+            }
+
+            return ops.Count(x => Eval(x));
+        }
+    }
+
+    internal class RopeBridgeCalibrator
+    {
+        List<Calibration> calibrations = new();
+        public void ParseInput(List<string> lines)
+            => lines.ForEach(x => calibrations.Add(new Calibration(x)));
+
+        long FindSum()
+        { 
+            var possibleCalibrations = calibrations.Where(x => x.FindWaysToSolve() > 0).ToList();
+            return possibleCalibrations.Sum(x => x.TestValue);
+        }
+
+        public long Solve(int part = 1)
+            => FindSum();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace AoC24
         {
             int day = 7;
             int part = 1;
-            bool test = !false;
+            bool test = false;
             int testNum = 0;
 
             string input = "./Input/day" + day.ToString("00");
@@ -84,9 +84,9 @@ namespace AoC24
         static string day7(string input, int part)
         {
             var lines = File.ReadAllLines(input).ToList();
-            //Day07 daySolver = new();
-            //daySolver.ParseInput(lines);
-            return ""; // daySolver.Solve(part).ToString();
+            Day07.RopeBridgeCalibrator daySolver = new();
+            daySolver.ParseInput(lines);
+            return daySolver.Solve(part).ToString();
         }
     }
 }
