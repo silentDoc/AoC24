62323c91bf9378e419235ec3e7851c395c687f29
silentDoc <ramon78t@yahoo.es>
2024-12-08 09:39:19 +0100

    Day 8 - Part 2 Completed. A bit dirty, refactor later

--- Day08/AntennaPlacer.cs
+++ Day08/AntennaPlacer.cs
@@ -37,16 +37,53 @@ namespace AoC24.Day08
                 }
         }
 
-        int FindAntinodes()
+        void BuildAntinodeLine(char antenna)
+        {
+            var locations = map.Keys.Where(x => map[x] == antenna).ToList();
+
+            if (locations.Count() == 1)
+                return;
+
+            for (int i = 0; i < locations.Count() - 1; i++)
+                for (int j = i + 1; j < locations.Count(); j++)
+                {
+                    var dif = locations[j] - locations[i];
+
+                    var antinode1 = locations[j] + dif;
+                    var antinode2 = locations[i] - dif;
+
+                    antinodes.Add(locations[j]);
+                    antinodes.Add(locations[i]);
+
+                    while (map.ContainsKey(antinode1))
+                    {
+                        antinodes.Add(antinode1);
+                        antinode1 += dif;
+                    }
+
+                    while (map.ContainsKey(antinode2))
+                    {
+                        antinodes.Add(antinode2);
+                        antinode2 -= dif;
+                    }
+                }
+        }
+
+        int FindAntinodes(int part = 1)
         {
             var antennas = map.Values.Where(x => x != '.').ToHashSet();
             foreach (var antenna in antennas)
-                BuildAntinode(antenna);
-            
+                if(part == 1)
+                    BuildAntinode(antenna);
+                else
+                    BuildAntinodeLine(antenna);
+
             return antinodes.Count();
         }
 
+
+
         public int Solve(int part = 1)
-            => FindAntinodes();
+            => FindAntinodes(part);
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace AoC24
         static void Main(string[] args)
         {
             int day = 8;
-            int part = 1;
+            int part = 2;
             bool test = false;
             int testNum = 0;
 
