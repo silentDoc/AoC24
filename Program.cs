b8d51643633624862ef826ecbb530ede393a4215
silentDoc <ramon78t@yahoo.es>
2024-12-09 06:24:03 +0100

    Day 9 - Readme, main and inputs

--- Program.cs
+++ Program.cs
@@ -6,9 +6,9 @@ namespace AoC24
     {
         static void Main(string[] args)
         {
-            int day = 8;
-            int part = 2;
-            bool test = false;
+            int day = 9;
+            int part = 1;
+            bool test = !false;
             int testNum = 0;
 
             string input = "./Input/day" + day.ToString("00");
@@ -27,6 +27,7 @@ namespace AoC24
                 6 => day6(input, part),
                 7 => day7(input, part),
                 8 => day8(input, part),
+                9 => day9(input, part),
                 _ => throw new ArgumentException("Wrong day number - unimplemented")
             };
             st.Stop();
@@ -96,6 +97,14 @@ namespace AoC24
             Day08.AntennaPlacer daySolver = new();
             daySolver.ParseInput(lines);
             return daySolver.Solve(part).ToString();
+        }
+
+        static string day9(string input, int part)
+        {
+            var lines = File.ReadAllLines(input).ToList();
+            //Day09 daySolver = new();
+            //daySolver.ParseInput(lines);
+            return "";//daySolver.Solve(part).ToString();
         }
     }
 }
