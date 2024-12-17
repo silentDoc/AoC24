39ac75c4495b0e51f3dabfec9fa56f72b8e36142
silentDoc <ramon78t@yahoo.es>
2024-12-17 08:31:01 +0100

    Day 17 - Part 1 Completed. About time we got a computer emulator :)

---
+++ Day17/ThreeBitComputer.cs
@@ -0,0 +1,126 @@
+﻿using AoC24.Common;
+
+namespace AoC24.Day17
+{
+    internal class ThreeBitComputer
+    {
+        const int Ins_Adv = 0;
+        const int Ins_Bxl = 1;
+        const int Ins_Bst = 2;
+        const int Ins_Jnz = 3;
+        const int Ins_Bxc = 4;
+        const int Ins_Out = 5;
+        const int Ins_Bdv = 6;
+        const int Ins_Cdv = 7;
+
+        Dictionary<char, int> reg = new();
+        List<int> sourceCode = new();
+        List<int> outputBuffer = new();
+
+        public void ParseInput(List<string> lines)
+        {
+            reg['A'] = int.Parse(lines[0].Replace("Register A: ", ""));
+            reg['B'] = int.Parse(lines[1].Replace("Register B: ", ""));
+            reg['C'] = int.Parse(lines[2].Replace("Register C: ", ""));
+            sourceCode = lines[4].Replace("Program: ", "").Split(',').Select(int.Parse).ToList();
+        }
+
+        int ComboOperand(int operand)
+            => operand switch
+            {
+                0 => operand,
+                1 => operand,
+                2 => operand,
+                3 => operand,
+                4 => reg['A'],
+                5 => reg['B'],
+                6 => reg['C'],
+                _ => throw new Exception("Invalid operand")
+            };
+        
+
+        int ExAdv(int insPtr)
+        {
+            int operand = ComboOperand(sourceCode[insPtr + 1]);
+            int num = reg['A'];
+            int den = (int) Math.Pow(2,operand);
+            reg['A'] = num / den;
+            return insPtr + 2;
+        }
+
+        int ExBxl(int insPtr)
+        {
+            int operand = sourceCode[insPtr + 1];
+            reg['B'] = reg['B'] ^ operand;
+            return insPtr + 2;
+        }
+
+        int ExBst(int insPtr)
+        {
+            int operand = ComboOperand(sourceCode[insPtr + 1]);
+            reg['B'] = MathHelper.Modulo(operand,8);
+            return insPtr + 2;
+        }
+
+        int ExJnz(int insPtr)
+            => reg['A'] == 0 ? insPtr + 2 : sourceCode[insPtr + 1];
+
+        int ExBxc(int insPtr)
+        {
+            reg['B'] = reg['B'] ^ reg['C'];
+            return insPtr + 2;
+        }
+
+        int ExOut(int insPtr)
+        {
+            int operand = ComboOperand(sourceCode[insPtr + 1]);
+            operand = MathHelper.Modulo(operand, 8);
+            outputBuffer.Add(operand);
+            return insPtr + 2;
+        }
+
+        int ExBdv(int insPtr)
+        {
+            int operand = ComboOperand(sourceCode[insPtr + 1]);
+            int num = reg['A'];
+            int den = (int)Math.Pow(2, operand);
+            reg['B'] = num / den;
+            return insPtr + 2;
+        }
+
+        int ExCdv(int insPtr)
+        {
+            int operand = ComboOperand(sourceCode[insPtr + 1]);
+            int num = reg['A'];
+            int den = (int)Math.Pow(2, operand);
+            reg['C'] = num / den;
+            return insPtr + 2;
+        }
+
+        int ExIns(int insPtr)
+            => sourceCode[insPtr] switch
+            {
+                Ins_Adv => ExAdv(insPtr),
+                Ins_Bxl => ExBxl(insPtr),
+                Ins_Bst => ExBst(insPtr),
+                Ins_Jnz => ExJnz(insPtr),
+                Ins_Bxc => ExBxc(insPtr),
+                Ins_Out => ExOut(insPtr),
+                Ins_Bdv => ExBdv(insPtr),
+                Ins_Cdv => ExCdv(insPtr)
+            };
+
+        string RunProgram()
+        {
+            int insPtr = 0;
+
+            while(insPtr<sourceCode.Count)
+                insPtr = ExIns(insPtr);
+
+            return string.Join(',', outputBuffer);
+        }
+
+        public string Solve(int part = 1)
+            => RunProgram();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,8 +8,8 @@ namespace AoC24
         {
             int day = 17;
             int part = 1;
-            bool test = !false;
-            int testNum = 1;
+            bool test = false;
+            int testNum = 0;
 
             string input = "./Input/day" + day.ToString("00");
             input += (test) ? "_test" + (testNum > 0 ? testNum.ToString() : "") + ".txt" : ".txt";
@@ -174,9 +174,9 @@ namespace AoC24
         static string day17(string input, int part)
         {
             var lines = File.ReadAllLines(input).ToList();
-            //Day17 daySolver = new();
-            //daySolver.ParseInput(lines);
-            return ""; // daySolver.Solve(part).ToString();
+            Day17.ThreeBitComputer daySolver = new();
+            daySolver.ParseInput(lines);
+            return daySolver.Solve(part).ToString();
         }
     }
 }
