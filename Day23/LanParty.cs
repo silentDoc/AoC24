b09b63ec701c408056e0c79dbbcbbd5b4738c4a8
silentDoc <ramon78t@yahoo.es>
2024-12-23 08:48:50 +0100

    Day 23 - Part 1 Completed. Lan party !

---
+++ Day23/LanParty.cs
@@ -0,0 +1,48 @@
+﻿namespace AoC24.Day23
+{
+    internal class LanParty
+    {
+        Dictionary<string, HashSet<string>> conns = new();
+
+        void ParseLine(string line)
+        {
+            var comps = line.Split("-");
+            if (!conns.ContainsKey(comps[0]))
+                conns[comps[0]] = new();
+            if (!conns.ContainsKey(comps[1]))
+                conns[comps[1]] = new();
+            conns[comps[0]].Add(comps[1]);
+            conns[comps[1]].Add(comps[0]);
+        }
+
+        public void ParseInput(List<string> input)
+            => input.ForEach(ParseLine);
+
+        int Find3Groups()
+        {
+            HashSet<string> groups = new();
+
+            foreach (var computer in conns.Keys)
+            {
+                var connectedComputers = conns[computer];
+                foreach (var neighbor in connectedComputers)
+                {
+                    var commonPCs = connectedComputers.Intersect(conns[neighbor]).ToHashSet();
+                    
+                    if (commonPCs.Count == 0)
+                        continue;
+
+                    var possibleGroups = commonPCs.Select(x => new List<string>([computer, neighbor, x])).ToList();
+                    var strPossibleGroups = possibleGroups.Select(x => string.Join('-', x.OrderBy(y => y))).ToList();
+                    strPossibleGroups.ForEach(x => groups.Add(x));
+                }
+            }
+
+            var listGroups = groups.ToList().OrderBy(x => x).ToList();
+            return listGroups.Count(x => x.IndexOf("-t") != -1 || x.StartsWith("t"));
+        }
+
+        public int Solve(int part = 1)
+            => Find3Groups();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace AoC24
         {
             int day = 23;
             int part = 1;
-            bool test = !false;
+            bool test = false;
             int testNum = 0;
 
             string input = "./Input/day" + day.ToString("00");
@@ -228,9 +228,9 @@ namespace AoC24
         static string day23(string input, int part)
         {
             var lines = File.ReadAllLines(input).ToList();
-            //Day23 daySolver = new();
-            //daySolver.ParseInput(lines);
-            return ""; // daySolver.Solve(part).ToString();
+            Day23.LanParty daySolver = new();
+            daySolver.ParseInput(lines);
+            return daySolver.Solve(part).ToString();
         }
     }
 }
